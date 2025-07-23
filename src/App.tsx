35dc668760ddf26870e8f2f3534c31c02012fa49
yeat <210401058@ogr.ikc.edu.tr>
2025-07-23 11:22:14 +0300

    deneme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AccountModule from './modules/AccountModule';
 
-function App() {
+const App: React.FC = () => {
+  const [activeModule, setActiveModule] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ padding: 20 }}>
+        <h1>🏦 PocketBank - Modül Seçici</h1>
+        <button onClick={() => setActiveModule(true)}>Modül 2 - Account Management</button>
+        <hr />
+        {activeModule && <AccountModule />}
+      </div>
   );
-}
+};
 
 export default App;
